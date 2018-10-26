f64699dd9d24d84aa84ced08346d1ad7af3ff63e
Jay Goss <jayspgoss@gmail.com>
2018-10-25 20:27:08 -0400

    onclick added

--- assets/js/ebay.js
+++ assets/js/ebay.js
@@ -1,8 +1,12 @@
-    var appid = "JayGoss-Marvelti-PRD-d7f3df809-292061ff";
-    keyword = "spiderman"
-    var queryURL = `https://cors-anywhere.herokuapp.com/http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=JayGoss-Marvelti-PRD-d7f3df809-292061ff&siteid=0&version=967&QueryKeywords=` + keyword + `&AvailableItemsOnly=true&MaxEntries=5`
-   
-    $.ajax({
+    // keyword = "spiderman"
+       
+    $(document).on("click", ".responsive-img", function() {
+      var keyword = $(this).attr("title");
+
+      var queryURL = `https://cors-anywhere.herokuapp.com/http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=JayGoss-Marvelti-PRD-d7f3df809-292061ff&siteid=0&version=967&QueryKeywords=` + keyword + `&AvailableItemsOnly=true&MaxEntries=4`
+      var appid = "JayGoss-Marvelti-PRD-d7f3df809-292061ff";
+
+      $.ajax({
       url: queryURL,
       method: "GET",
       data: { format: "json"},
@@ -10,7 +14,28 @@
       dataType: 'JSON',
  
     }).then(function(response) {
-      console.log("YES");
       console.log(response);
+      $("#ebay-results").empty();
+      var results = response.Product;
 
+     for (i = 0; i < results.length; i++) {
+      var ebayDiv = $("<div>");
+
+      var comicPicture = results[i].StockPhotoURL;
+      var picture = $("<img>")
+      picture.attr("src", comicPicture);
+
+      var comicTitle = results[i].Title;
+      var title = $("<p>").text(comicTitle);
+
+      var auctionPage = results[i].DetailsURL;
+      var auctionLink = $("<a>").text(auctionPage);
+      auctionLink.attr("href", auctionPage);
+   
+      ebayDiv.append(picture, title, auctionLink);
+ 
+      $("#ebay-results").prepend(ebayDiv);
+
+    }
+      });
     });
